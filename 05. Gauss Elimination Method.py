9e4ed7f5f0f4a88375e777360adf19bb72d314f3
Sutharya <120108404+ssutharya@users.noreply.github.com>
2024-03-11 02:29:09 -0700

    Update 05. Gauss Elimination Method.py

--- 05. Gauss Elimination Method.py
+++ 05. Gauss Elimination Method.py
@@ -1,26 +1,34 @@
 #Write a program to solve system of linear eqns using gauss elimination method
 
 def gauss(matrix):
-  n = len(matrix)                                                   #length of matrix as in rows
+    n = len(matrix)                                                             # length of matrix as in rows
 
-  for i in range(n):
-    pivot = matrix[i][i]                                            #choosing pivot: 00, 11, 22...
+    for i in range(n):
+        pivot = matrix[i][i]                                                    # choosing pivot: 00, 11, 22...
 
-    for j in range(n):
-      if j != i:                                                    #avoiding the pivot elements
-        eq = matrix[j][i] / pivot                                   # the equation to use for making the other elements 0
-        for k in range(n+1):                                        # changing the elements along with the b vector
-          matrix[j][k] -= eq * matrix[i][k]
+        for j in range(n):
+            if j != i:                                                          # avoiding the pivot elements
+                eq = matrix[j][i] / pivot                                       # the equation to use for making the other elements 0
+                for k in range(n + 1):                                          # changing the elements along with the b vector
+                    matrix[j][k] -= eq * matrix[i][k]
 
-    pivot = matrix[i][i]                                           # dividing pivot by itself so we get the solutions
-    for j in range(n+1):
-      matrix[i][j] /= pivot
+        pivot = matrix[i][i]                                                    # dividing pivot by itself so we get the solutions
+        for j in range(n + 1):
+            matrix[i][j] /= pivot
+
+    solutions = [0] * n
+    for i in range(n - 1, -1, -1):
+        solutions[i] = matrix[i][n]
+        for j in range(i + 1, n):
+            solutions[i] -= matrix[i][j] * solutions[j]
+
+    return solutions
 
 matrix = [[2, 2, 1, 6],
-         [4, 2, 3, 4],
-         [1, 1, 1, 0]]
+          [4, 2, 3, 4],
+          [1, 1, 1, 0]]
 
-gauss(matrix)
-    
-for i in range(len(matrix)):
-  print("x", i+1, "=", matrix[i][-1])
+solutions = gauss(matrix)
+
+for i, solution in enumerate(solutions):
+    print("x", i + 1, "=", solution)
